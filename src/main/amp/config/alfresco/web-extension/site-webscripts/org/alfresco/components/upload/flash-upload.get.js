19fc88c4481c1dc686e373da5db15f7780268de1
josafa <josafa@jdebian.uesb.br>
2014-12-09 16:10:05 -0300

    Alterar "site-webscripts".

---
+++ src/main/amp/config/alfresco/web-extension/site-webscripts/org/alfresco/components/upload/flash-upload.get.js
@@ -0,0 +1,44 @@
+/**
+ * Custom content types
+ */
+function getContentTypes()
+{
+   // TODO: Data webscript call to return list of available types
+   var contentTypes = [
+   {
+      id: "cm:content",
+      value: "type.cm_content"
+   }, {
+		id : "sif:documento_admissional",
+		value : "type.sif_documento_admissional"
+	}, {
+		id : "sif:ficha_admissional",
+		value : "type.sif_ficha_admissional"
+	}, {
+		id : "sif:processo",
+		value : "type.sif_processo"
+	}, {
+		id : "sif:portaria",
+		value : "type.sif_portaria"
+	}, {
+		id : "sif:frequencia",
+		value : "type.sif_frequencia"
+	} ];
+
+   return contentTypes;
+}
+
+model.contentTypes = getContentTypes();
+
+function main()
+{
+   // Widget instantiation metadata...
+   var flashUpload = {
+      id : "FlashUpload", 
+      name : "Alfresco.FlashUpload"
+   };
+   model.widgets = [flashUpload];
+}
+
+main();
+

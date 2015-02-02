9b0db4a946fe563f53a64cf48b02e9e6a55aab4f
josafa <josafa@jdebian.uesb.br>
2015-02-02 19:04:07 -0300

    Alterações em run.sh.

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 springloadedfile=~/.m2/repository/org/springframework/springloaded/1.2.0.RELEASE/springloaded-1.2.0.RELEASE.jar
 
 if [ ! -f $springloadedfile ]; then
-mvn validate -Psetup
+/opt/Apache/apache-maven-3.2.3/bin/mvn validate -Psetup
 fi
 
-MAVEN_OPTS="-javaagent:$springloadedfile -noverify" mvn integration-test -Pamp-to-war
+MAVEN_OPTS="-javaagent:$springloadedfile -noverify" /opt/Apache/apache-maven-3.2.3/bin/mvn integration-test -Pamp-to-war

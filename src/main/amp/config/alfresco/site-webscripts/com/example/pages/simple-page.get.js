c9ec3468e358e09ec9cbfbbd6e261ff50ba1775f
josafa <josafa@debian>
2015-03-01 01:36:11 -0300

    Testes Aikau.

--- src/main/amp/config/alfresco/site-webscripts/com/example/pages/simple-page.get.js
+++ src/main/amp/config/alfresco/site-webscripts/com/example/pages/simple-page.get.js
@@ -1,27 +1,75 @@
-model.jsonModel = {
-    widgets: [{
-        id: "SET_PAGE_TITLE",
-        name: "alfresco/header/SetTitle",
-        config: {
-            title: "This is a simple page"
-        }
-    }, 
-    {
-        id: "MY_HORIZONTAL_WIDGET_LAYOUT",
-        name: "alfresco/layout/HorizontalWidgets",
-        config: {
-            widgetWidth: 50,
-            widgets: [
-                {
-                    name: "alfresco/logo/Logo",
-                    config: {
-                        logoClasses: "alfresco-logo-only"
-                    }
-                },
-                {
-                  name: "example/widgets/TemplateWidget"
-                }
-            ]
-        }
-    }]
+var warning = {
+	name : "alfresco/header/Warning",
+	config : {
+		renderFilterMethod : "ALL",
+		renderFilter : [ {
+			target : "groupMemberships",
+			property : "GROUP_ALFRESCO_ADMINISTRATORS",
+			renderOnAbsentProperty : true,
+			values : [ false ]
+		}, {
+			target : "groupMemberships",
+			property : "GROUP_EXAMPLE",
+			renderOnAbsentProperty : true,
+			values : [ false ]
+		} ],
+		warnings : [ {
+			message : "You aren't in right group",
+			level : 3
+		} ]
+	}
 };
+
+var menuBar = {
+	name : "alfresco/menus/AlfMenuBar",
+	config : {
+		renderFilterMethod : "ANY",
+		renderFilter : [ {
+			target : "groupMemberships",
+			property : "GROUP_ALFRESCO_ADMINISTRATORS",
+			values : [ true ]
+		}, {
+			target : "groupMemberships",
+			property : "GROUP_EXAMPLE",
+			values : [ true ]
+		} ],
+		widgets : []
+	}
+};
+
+menuBar.config.currentItem = {
+	test : {
+		value : "show"
+	}
+};
+
+var menuBarItems = [ {
+	name : "alfresco/menus/AlfMenuBarItem",
+	config : {
+		label : "Should Appear",
+		renderFilter : [ {
+			property : "test.value",
+			values : [ "show" ]
+		} ]
+	}
+}, {
+	name : "alfresco/menus/AlfMenuBarItem",
+	config : {
+		label : "Should NOT appear",
+		renderFilter : [ {
+			property : "test.value",
+			values : [ "visible" ]
+		} ]
+	}
+} ];
+menuBar.config.widgets = menuBarItems;
+
+model.jsonModel = {
+	widgets : [ {
+		name : "alfresco/layout/VerticalWidgets",
+		config : {
+			widgets : [ warning, menuBar ]
+		}
+	} ]
+};
+model.jsonModel.groupMemberships = user.properties["alfUserGroups"];
